78b71ba6cd2de79698ee2016bab1d70d65584bb3
j-lrx <49448040+j-lrx@users.noreply.github.com>
2023-03-14 23:03:46 +0100

    Update subnet.tf

--- 1/subnet.tf
+++ 1/subnet.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "subnet"{
     name             = "subnet-1"
     network          = google_compute_network.vpc.self_link
-    ip_cidr_range    = "10.0.0.1/20"
+    ip_cidr_range    = "192.168.10.0/24"
 }
