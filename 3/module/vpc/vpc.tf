684f47b9dd23a230f45bd0adefb71c25bb8355aa
j-lrx <49448040+j-lrx@users.noreply.github.com>
2023-03-15 13:39:34 +0100

    Delete vpc.tf

--- 3/module/vpc/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_network" "vpc"{
-    name                     = "${var.name}"
-    project                  = "${var.project}"
-    auto_create_subnetworks  = "${var.auto_create_subnetworks}"
-    description              = "${var.description}"
-}
